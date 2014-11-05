d89eb44b6d168976d1a3864d8c7f38aa7b3d3531
Akiva Levy <akiva@sixthirteen.co>
2014-11-05 13:59:06 -0800

    Update Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,48 +4,41 @@ var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var bogan = require('boganipsum');
 var jade = require('gulp-jade');
-var clean = require('gulp-clean');
-var connect = require('gulp-connect');
+var del = require('del');
+var webserver = require('gulp-webserver');
 var autoprefix = require('gulp-autoprefixer');
 
-gulp.task('clean', function() {
-  return gulp.src(
-    [
-      './test/**/*.html',
-      './test/**/*.css'
-    ],
-    { 'read': false }
-  )
-    .pipe(clean());
+gulp.task('clean:html', function() {
+  del('./test/**/*.html');
 });
 
-gulp.task('stylus', function() {
+gulp.task('clean:css', function() {
+  del('./test/**/*.css');
+});
+
+gulp.task('stylus', ['clean:css'], function() {
   gulp.src('./test/css/app.styl')
     .pipe(stylus())
     .pipe(autoprefix('last 1 version'))
-    .pipe(gulp.dest('./test/css'))
-    .pipe(connect.reload());
+    .pipe(gulp.dest('./test/css'));
 });
 
-gulp.task('jade', function() {
-  var LOCALS = { bogan: bogan };
-
+gulp.task('jade', ['clean:html'], function() {
   gulp.src('./test/**/*.jade')
-    .pipe(jade({ locals: LOCALS }))
-    .pipe(gulp.dest('./test/'))
-    .pipe(connect.reload());
+    .pipe(jade({ locals: { bogan: bogan } }))
+    .pipe(gulp.dest('./test'));
 });
 
-gulp.task('server', function() {
-  connect.server({
-    root: 'test',
-    livereload: true
-  });
+gulp.task('webserver', function() {
+  gulp.src('./test')
+    .pipe(webserver({
+      livereload: true
+    }));
 });
 
-gulp.task('watch', ['server'], function() {
-  gulp.watch('**/*.styl', ['stylus']);
+gulp.task('watch', ['default', 'webserver'], function() {
+  gulp.watch('./**/*.styl', ['stylus']);
   gulp.watch('./test/*.jade', ['jade']);
 });
 
-gulp.task('dev', ['clean', 'stylus', 'jade', 'watch']);
+gulp.task('default', ['stylus', 'jade']);
